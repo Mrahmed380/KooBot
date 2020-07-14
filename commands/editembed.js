96caae3edf60730d13ec5a7d162ad1ae3caa1f09
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-14 16:18:13 +0800

    Create editembed.js

---
+++ commands/editembed.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+module.exports.run = async (bot, message, args) => {
+  if(message.author.id !== config.ownerID) return;
+  let embed6 = new Discord.RichEmbed()
+  .setDescription(`:no_entry_sign: ${message.author.username}, 這個指定只適用於 RealKoolisw`)
+  .setColor('RED')
+  const msgId = args[0]
+  
+  let embed = new Discord.RichEmbed()
+  .setTitle('**Finish**')
+  .setDescription('edit embed')
+  .setColor('BLUE')
+
+  
+  
+  message.delete()
+  
+  message.channel.fetchMessages({around: msgId, limit: 1})
+    .then(msg => {
+        const fetchedMsg = msg.first();
+        fetchedMsg.edit(embed);
+    });
+}
