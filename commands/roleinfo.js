7b9418caedcfebbdd60a620c1b80c7b8f1bf5a9b
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-14 16:33:49 +0800

    Update roleinfo.js

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -11,14 +11,14 @@ module.exports.run = async (bot, message, args) => {
   
   
   let embed3 = new Discord.RichEmbed()
-  .setTitle('**酷斯軍團 Koolisw Nation 詳細入會指引及伺服器教程**')
-  .setDescription('歡迎來到Koolisw以及Draconian Workshop官方Discord伺服器\n\n以下內容就是對於我們伺服器的主要資訊以及詳細身份組內容，並有效地可以幫助一些新加入的成員，再加入本伺服器之前請細閱這些指引及資訊。')
+  .setTitle('**酷斯軍團 Koolisw Nation 伺服器所有身份組**')
+  .setDescription('歡迎來到Koolisw以及Draconian Workshop官方Discord伺服器\n\n以下內容就是對於我們伺服器的所有身份組,並會明確列出身份組的獲取方法以及待遇。')
   .setColor('BLUE')
   
   
   let embed7 = new Discord.RichEmbed()
-  .setTitle('**伺服器身分組 Server Roles**')
-  .setDescription('以下就是我們本群組的所有身份組, 等級身份組會在下面, 並會列出一切的申請方法\n\n**__所有身份組 (由低至高)__**\n\n<@&687220191121375236> - 當所有成員完成驗證的時候可以獲得這個身份組\n\n<@&698904930320253006> - 當你在Roblox群或者在Discord群組舉辦2次nitro classic即可獲得\n\n<@&698370818283012096> - 每個月支付本群組100 robux, 即可獲得, 當沒有續期的時候本身份組會被取消\n\n<@&698097757998678066> - 在YouTube頻道獲得至少500訂閱, 2000次觀看次數, 即可申請\n\n <@&729001360950951977> - 在Discord伺服器, 至少有100人以及50人同時上線, 該discord伺服器的群主以及管理層該可申請, 每個Discord伺服器最高限度可以申請3-5人\n\n<@&724999998479007847> - 該為本群的管理層人員,有助維護治安以及其他事項, 包括: 群組協助,初級管理員,群管, 申請對象: 完全懂得使用機械人指令,至少加入本群三星期,經常上線經常聊天,處事公正,不怕得罪別人\n\n**__等級身份組 (由低至高)__**\n\n**等級3** <@&723457302996189245> - 擁有對於高級版抽獎的參與資格, jailbreak保險箱/skyblock等等遊戲內抽獎除外\n\n**等級10** <@&691991846888407071> - 擁有高音質的專屬語音室, 解鎖更多頻道, 未來會加更多\n\n**__懲罰角色__**\n\n<@&699260818260361257> - 當你違反本群組規則的時候, 第一次你會獲得這個角色\n<@&699260882366234725> - 第二次違反\n<@&699260925999448144> - 最後警告, 在一次違反直接封鎖\n\n有更多自定義指派的role, 請前往<#698383699346980885>')
+  .setTitle('**身份組列表 Role List**')
+  .setDescription('**__所有身份組 (由低至高)__**\n\n<@&687220191121375236> - 當所有成員完成驗證的時候可以獲得這個身份組\n\n<@&698904930320253006> - 當你在Roblox群或者在Discord群組舉辦2次nitro classic即可獲得\n\n<@&698370818283012096> - 每個月支付本群組100 robux, 即可獲得, 當沒有續期的時候本身份組會被取消\n\n<@&698097757998678066> - 在YouTube頻道獲得至少500訂閱, 2000次觀看次數, 即可申請\n\n <@&729001360950951977> - 在Discord伺服器, 至少有100人以及50人同時上線, 該discord伺服器的群主以及管理層該可申請, 每個Discord伺服器最高限度可以申請3-5人\n\n<@&724999998479007847> - 該為本群的管理層人員,有助維護治安以及其他事項, 包括: 群組協助,初級管理員,群管, 申請對象: 完全懂得使用機械人指令,至少加入本群三星期,經常上線經常聊天,處事公正,不怕得罪別人\n\n**__等級身份組 (由低至高)__**\n\n**等級3** <@&723457302996189245> - 擁有對於高級版抽獎的參與資格, jailbreak保險箱/skyblock等等遊戲內抽獎除外\n\n**等級10** <@&691991846888407071> - 擁有高音質的專屬語音室, 解鎖更多頻道, 未來會加更多\n\n**__懲罰角色__**\n\n<@&699260818260361257> - 當你違反本群組規則的時候, 第一次你會獲得這個角色\n<@&699260882366234725> - 第二次違反\n<@&699260925999448144> - 最後警告, 在一次違反直接封鎖\n\n有更多自定義指派的role, 請前往<#698383699346980885>\n\n如果你想申請以上身份組, 請到<#714024761092603944>頻道按下表情符支援及獲取')
   .setColor('BLUE')
   
   
@@ -29,5 +29,5 @@ module.exports.run = async (bot, message, args) => {
   
   
   message.delete()
-  message.channel.send(embed3).then(message.channel.send(embed2).then(message.channel.send(embed4).then(message.channel.send(embedwp).then(message.channel.send(embed7).then(message.channel.send(verifyiteoms))))))
+  message.channel.send(embed3).then(message.channel.send(embed7).then(message.channel.send(embed4).then(message.channel.send(embedwp).then(message.channel.send(embed7).then(message.channel.send(verifyiteoms))))))
 }
