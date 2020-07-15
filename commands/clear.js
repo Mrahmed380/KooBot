27241ab8dcf61dda307dcda4959b92c2810f4263
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-15 20:43:52 +0800

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
   let logs = message.guild.channels.find('name', config.logsChannel);
   let embed6 = new Discord.RichEmbed()
-  .setDescription(`:no_entry_sign: ${message.author.username}, 你沒有權限去執行這個指令`)
+  .setDescription(`:no_entry_sign: ${message.author.username}, Missing Permission`)
   .setColor('RED')
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(embed6).then(msg=>msg.delete(5000));
   if(!args[0]) return message.channel.send(`<:no:565766936189861889> Use: **\`?clear <1 - 100>\`**`).then(msg=>msg.delete(5000));
@@ -13,11 +13,17 @@ module.exports.run = async (bot, message, args) => {
   message.channel.bulkDelete(args[0]).then(() => {
   let embed = new Discord.RichEmbed()
   .setColor('GREEN')
-  .setTitle("清除操作")
-  .addField("執行者", `<@${message.author.id}> | **${message.author.username}#${message.author.discriminator}**`)
-  .addField("已清除", `**${args[0]}**`)
-  .addField("頻道", `${message.channel} | **${message.channel.name}**`)
+  .setTitle("Clear Action")
+  .addField("User", `<@${message.author.id}> | **${message.author.username}#${message.author.discriminator}**`)
+  .addField("Cleared", `**${args[0]}**`)
+  .addField("Channel", `${message.channel} | **${message.channel.name}**`)
+  
+  let kntlembed = new Discord.RichEmbed()
+  .setColor('GREEN')
+  .setDescription(`Cleared `${args[0]}` Message in **${args[0]}**`)
+  
   logs.send(embed);
+  message.channel.send(kntlembed).then(msg=>msg.delete(30000))
 });
 
 }
