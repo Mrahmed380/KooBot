8d74bfec8949085c26c14cc8db0b526803fba1e1
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-15 20:49:07 +0800

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -8,13 +8,13 @@ exports.run = async(bot, msg, args) => {
   let reason = args.slice(1).join(' ');
   let logs = msg.guild.channels.find('name', config.logsChannel);
   let mmqembed = new Discord.RichEmbed()
-  .setDescription(`:no_entry_sign: ${msg.author.username}, 你沒有權限去執行這個指令`)
+  .setDescription(`:no_entry_sign: ${msg.author.username}, Missing Permission`)
   .setColor('#FFFF00')
   if (!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send(mmqembed).then(msg=>msg.delete(5000));
   
   let kntlembed = new Discord.RichEmbed()
-  .setTitle("指令: +ban")
-  .setDescription("你看到這條信息的話，可能你使用方法出錯了。\n\n**內容功能:** Ban一個成員\n**使用方法:** +ban [用戶] [原因]\n**例子:** +ban @RealKoolisw Noob")
+  .setTitle("Command: +ban")
+  .setDescription("Wrong Usage!。\n\n**Function:** Ban a member\n**Usage:** +ban [User] [Reason]\n**Example:** +ban @RealKoolisw Noob")
   .setColor('RED')
   if (!banTaged) {
     msg.delete()
@@ -27,7 +27,7 @@ exports.run = async(bot, msg, args) => {
   .setColor('RED')
   if (!reason) {
     msg.delete()
-    return msg.channel.send(asuembed).then(msg=>msg.delete(30000))
+    return msg.channel.send(kntlembed).then(msg=>msg.delete(30000))
   }
   
   let lombed = new Discord.RichEmbed()
@@ -37,20 +37,20 @@ exports.run = async(bot, msg, args) => {
   
   let banEmbed = new Discord.RichEmbed()
   .setColor('RED')
-  .setAuthor(`Action Ban`, `https://images-ext-2.discordapp.net/external/bs8dWv1gtMJxSyxXMpiJSdYSH62d63BF8yuwMwl3fbM/http/icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-ban-icon.png`)
-  .addField("用戶", `<@${banTaged.user.id}> | **${banTaged.user.username}#${banTaged.user.discriminator}**`)
-  .addField("執行者", `<@${msg.author.id}> | **${msg.author.username}#${msg.author.discriminator}**`)
-  .addField("原因", `\`\`\`${reason}\`\`\``)
+  .setAuthor(`Action Ban`)
+  .addField("Target", `<@${banTaged.user.id}> | **${banTaged.user.username}#${banTaged.user.discriminator}**`)
+  .addField("User", `<@${msg.author.id}> | **${msg.author.username}#${msg.author.discriminator}**`)
+  .addField("Reason", `\`\`\`${reason}\`\`\``)
   .setTimestamp()
   .setFooter(`• Ban User Information`);//
   let bsuembed = new Discord.RichEmbed()
-  .setDescription(`<:tick:702386031361523723>成功封鎖 _**${banTaged.user.username}#${banTaged.user.discriminator}**_ | ${reason}！`)
+  .setDescription(`<:tick:702386031361523723> Banned _**${banTaged.user.username}#${banTaged.user.discriminator}**_ | ${reason}`)
   .setColor('#FFFF00')
   
   msg.delete()
   msg.channel.send(bsuembed);
   banTaged.ban(reason);
   logs.send(banEmbed);
-  banTaged.send(`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n                                 **BAN信息**\n\n您在**${msg.guild.name}**已被禁止\n由 **${msg.author.username}**封鎖, 原因為 for **${reason}**\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`)
+  banTaged.send(`You are banned **${msg.guild.name}** by **${msg.author.username}**, Reason : **${reason}**`)
   
 };
