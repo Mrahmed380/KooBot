571cfb1d531b8b012ae8dc23066b4a7038170cf3
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-15 22:03:36 +0800

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -2,19 +2,19 @@ const Discord = require("discord.js");
 const urban = require("urban");
 
 module.exports.run = async (bot, message, args) => {
-    if(args.length < 1) return message.reply("請輸入些小東西!");
+    if(args.length < 1) return message.reply("Type something!");
     let XD = args.join(" "); 
 
     urban(XD).first(json => {
-        if(!json) return message.reply("無法尋找!")
+        if(!json) return message.reply("Cannot Search!")
 
         let urbEmbed = new Discord.RichEmbed()
         .setColor("00ff00")
         .setTitle(json.word)
         .setDescription(json.definition)
-        .addField("同意", json.thumbs_up, true)
-        .addField("反對", json.thumbs_down, true)
-        .setFooter(`由${json.author}編寫`);
+        .addField("Agree", json.thumbs_up, true)
+        .addField("Disagree", json.thumbs_down, true)
+        .setFooter(`Written in Urban by ${json.author}`);
 
         message.channel.send(urbEmbed)
     });
