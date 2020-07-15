e5f9fb4b1ebfa746b411d6e0da2defbf164517dc
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-15 21:59:46 +0800

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
 
         //If the place entered is invalid
         if(result.length === 0) {
-            message.channel.send("**請輸入一個有效地區**")
+            message.channel.send("**Type an area**")
             return;
         }
 
@@ -22,15 +22,15 @@ module.exports.run = async (bot, message, args) => {
            .setAuthor(`Weather for ${current.observationpoint}`) //Shows the current location of the weater
            .setThumbnail(current.imageUrl) //Sets thumbnail of the embed
            .setColor(0x00AE86) //Sets the color of the embed
-           .addField("時區", `UTC${location.timezone}`, true) //Shows the timezone
-           .addField("Degree類型", location.degreetype, true) //Shows the degrees in Celcius
-           .addField("溫度", `${current.temperature}`, true)
-           .addField("感覺像", `${current.feelslike} Degrees`, true)
-           .addField("風速", current.winddisplay, true)
-           .addField("濕度", ` ${current.humidity}%`, true)
-           .addField("星期", `${current.day}`, true)
-           .addField("日期", `${current.date}`, true)
-           .setFooter(`©2020 Draconian Workshop | 這個指令由${message.author.username}#${message.author.discriminator}請求`)
+           .addField("Time Zone", `UTC${location.timezone}`, true) //Shows the timezone
+           .addField("Degree Type", location.degreetype, true) //Shows the degrees in Celcius
+           .addField("Temperature", `${current.temperature}`, true)
+           .addField("Feels like", `${current.feelslike} Degrees`, true)
+           .addField("Wind speed", current.winddisplay, true)
+           .addField("Wet Rate", ` ${current.humidity}%`, true)
+           .addField("Week", `${current.day}`, true)
+           .addField("Date", `${current.date}`, true)
+           .setFooter(`©2020 Draconian Workshop | Requested by ${message.author.username}#${message.author.discriminator}`)
            
            //Display when it's called
            message.channel.sendEmbed(embed)
