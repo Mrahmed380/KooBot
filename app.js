6cbbf4b273b88d9129a7ca64cfa1b8802729c04c
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-19 22:38:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const db = require("quick.db")
 const dbl = require("dblposter");
 const DBLPoster = new dbl(`
 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxMTkzNzU5OTk3NTA2MzU4NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk0MjkwODc0fQ.2dV7JhcntDs_trKMXf2ZY8Ys4DuuSmbj1cSaub7c2E0`);
-
+DBLPoster.bind(client);
 
 
 client.on("ready", () => {
