c05537c92fdca9ee1e0918980193fe748555f815
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-20 14:14:01 +0800

    Update and rename cosplay.js to holo.js

--- commands/holo.js
+++ commands/holo.js
@@ -4,12 +4,12 @@ const superagent = require('superagent')
 exports.run = (client, msg, args) => {
   if (msg.channel.nsfw === true) {
     superagent.get('https://nekobot.xyz/api/image')
-    .query({ type: 'cosplay'})
+    .query({ type: 'holo'})
     .end((err, response, body) => {
       let emb = new discord.RichEmbed()
                     .setImage(response.body.message)
                     .setColor("#00ff00")
-                    .setTitle("Cosplay here")
+                    .setTitle("Holo here")
                     .setFooter(`©2020 Draconian Workshop | This command requested by ${msg.author.username}#${msg.author.discriminator}`)
                               
                    msg.channel.send(emb)  
