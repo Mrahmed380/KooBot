930a85422b39f0b64f395ad031f69d92fa91a841
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-20 14:03:43 +0800

    Update 4knsfw.js

--- commands/4knsfw.js
+++ commands/4knsfw.js
@@ -5,8 +5,14 @@ exports.run = (client, msg, args) => {
   if (msg.channel.nsfw === true) {
     superagent.get('https://nekobot.xyz/api/image')
     .query({ type: '4k'})
-    .end((err, response) => {
-      msg.channel.send({ file: response.body.message });
+    .end((err, response, body) => {
+      let emb = new discord.RichEmbed()
+                    .setImage(response.body.message)
+                    .setColor("#00ff00")
+                    .setTitle("4K NSFW here")
+                    .setFooter(`©2020 Draconian Workshop | This command requested by ${msg.author.username}#${msg.author.discriminator}`)
+                              
+                   msg.channel.send(emb)  
     });
   } else {
     msg.channel.send("This isn't NSFW channel!")
