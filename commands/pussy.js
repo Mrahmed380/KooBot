aebda2b7303b0fc41ac05b156cfe26d76bb21032
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-20 13:43:41 +0800

    Update pussy.js

--- commands/pussy.js
+++ commands/pussy.js
@@ -1,15 +1,28 @@
-const discord = require('discord.js');
-const superagent = require('superagent')
+const Discord = require('discord.js');
+const superagent = require('superagent');
 
-exports.run = (client, msg, args) => {
-  if (msg.channel.nsfw === true) {
-    superagent.get('https://nekobot.xyz/api/image')
-    .query({ type: 'pussy'})
-    .end((err, response) => {
-      msg.channel.send({ file: response.body.message });
-    });
-  } else {
-    msg.channel.send("This isn't NSFW channel!")
-  }
+
+exports.run = async (client, message, args) => {
+    const { body } = await superagent
+    .get("https://nekos.life/api/v2/img/pussy");
+    
+    const embed = new Discord.RichEmbed()
+    .setColor("#ff9900")
+    .setTitle(`Heres your Pussy Image`)
+    .setImage(body.url) 
+    .setFooter(`© Draconian Workshop`);
+    message.channel.send({embed})
 };
 
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'ngif',
+    description: 'Neko Gifs OwO',
+    usage: 'ngif'
+  };
