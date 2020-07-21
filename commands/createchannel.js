8986071a20f4d7aa4844b83d9b6f79e9f45c823e
koolisw <65409152+koolisw@users.noreply.github.com>
2020-07-21 13:36:59 +0800

    Update createchannel.js

--- commands/createchannel.js
+++ commands/createchannel.js
@@ -3,9 +3,10 @@ exports.run = async (client, message, args) => { // eslint-disable-line no-unuse
     let embed6 = new Discord.RichEmbed()
   .setDescription(`:no_entry_sign: ${message.author.username}, Missing Permission`)
   .setColor('RED')
+    if (!message.member.hasPermissions("MANAGE_CHANNELS")) return message.channel.send(embed6).then(msg=>msg.delete(5000));
     if (!args[1]) return message.reply('You need to input the channel type!');
     if (!args[0]) return message.reply('You need to input the channel name!');
-    if (!message.member.hasPermissions("MANAGE_CHANNELS")) return message.channel.send(embed6).then(msg=>msg.delete(5000));
+    
     message.channel.send('I\'ve created the channel!').then(() => {
       message.guild.createChannel(args[1], args[0], []).catch((err) => {
         message.channel.send('There was an error!')
